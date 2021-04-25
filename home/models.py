fd12f39359d1f92086d5eadd6c6199f083749849
Ben Hampson <bjhampson@gmail.com>
2021-04-25 01:01:16 +0100

    basic Bulma CSS styling

--- home/models.py
+++ home/models.py
@@ -7,7 +7,20 @@ from wagtail.admin.edit_handlers import FieldPanel
 
 class HomePage(Page):
     body = RichTextField(blank=True)
+    hero_title = models.CharField(
+        max_length=120,
+        blank=True,
+        help_text="Main text displayed in the hero section over the background."
+    )
+    hero_subtitle = models.CharField(
+        max_length=300,
+        blank=True,
+        help_text="Subtitle text for the hero section."
+    )
 
+    # Configure the Admin interface
     content_panels = Page.content_panels + [
+        FieldPanel("hero_title"),
+        FieldPanel("hero_subtitle"),
         FieldPanel('body', classname="full"),
     ]
