2fa51292524e6e77b9be61d93b38c105faecff6c
Ben Hampson <bjhampson@gmail.com>
2021-05-28 00:31:13 +0100

    add favicon

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.urls import include, path
 from django.contrib import admin
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
@@ -15,6 +17,8 @@ urlpatterns = [
     path('documents/', include(wagtaildocs_urls)),
 
     path('search/', search_views.search, name='search'),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico'))),
+    
 
 ]
 
