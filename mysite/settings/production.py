1c14c50a7b614d3f9bfbe7cf50a3035e2fb20647
Ben Hampson <bjhampson@gmail.com>
2021-06-07 18:36:35 +0100

    update email backend for server environment

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -46,7 +46,7 @@ ALLOWED_HOSTS = ['*']
 
 # Email Settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.mailgun.org'
+EMAIL_HOST = 'smtp-relay.sendinblue.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get('MAILGUN_SMTP_LOGIN')
-EMAIL_HOST_PASSWORD = os.environ.get('MAILGUN_SMTP_PASSWORD')
+EMAIL_HOST_USER = env['SMTP_LOGIN']
+EMAIL_HOST_PASSWORD = env['SMTP_PASSWORD']
