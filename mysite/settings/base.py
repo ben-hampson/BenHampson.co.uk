97bfad4870d8a321bb127264ba996fc55f6ba485
Ben Hampson <bjhampson@gmail.com>
2021-06-07 15:53:51 +0100

    remove unused imports from base.py

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-import django_on_heroku
-import dj_database_url
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
